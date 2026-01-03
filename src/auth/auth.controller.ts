6e76db1751e14dac9184396109d79c4d5e1dce28
mohammad hadi <m.6336@yahoo.com>
2026-01-03 17:30:58 +0330

    added register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get,  } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+constructor(
+ private readonly authService: AuthService
+){}
+
+     @Get('register')
+    async register(){
+        return this.authService.register();
+    }
+}
